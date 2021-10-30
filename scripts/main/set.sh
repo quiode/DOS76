e10c94890d85518156515c9edc4fc6f6b65e10c5
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-10-30 18:52:13 +0200

    set update repo

--- scripts/main/set.sh
+++ scripts/main/set.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # This script gets all config files on this system and updates the repo.
+echo "\e[1;34mUpdating repo config files...\e[0m"
+# Constants
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Variables
+unalias cp
+
+# Updating repo
+cd "$SCRIPT_DIR" || exit
+git pull --rebase
